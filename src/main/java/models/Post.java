f7a16d07b592e0532338846143b3fcd4209f958d
JoyMbugua <wacumbugua@gmail.com>
2021-02-16 13:39:22 +0300

    date and timestamps

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,14 +1,21 @@
 package models;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Post {
     private String content;
     private static ArrayList<Post> instances = new ArrayList<>();
+    private boolean published;
+    private LocalDateTime createdAt;
+    private int id;
 
     public Post(String content){
         this.content = content;
         instances.add(this);
+        this.published = false;
+        this.createdAt = LocalDateTime.now();
+        this.id = instances.size();
     }
 
     public String getContent() {
@@ -21,5 +28,17 @@ public class Post {
 
     public static void clearAll(){
         instances.clear();
+    }
+
+    public boolean getPublished(){
+        return this.published;
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return this.createdAt;
+    }
+
+    public int getId(){
+        return id;
     }
 }
--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -4,6 +4,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.Assert.*;
 
 public class PostTest {
@@ -16,30 +18,55 @@ public class PostTest {
     public void tearDown() throws Exception {
     }
 
+    public Post setUpNewPost(){
+        return new Post("Day 1: intro");
+    }
     @Test
     public void newPostGetsCreatedCorrectly_true() throws Exception {
-        Post post = new Post("Day 1: intro");
+        Post post = setUpNewPost();
         assertEquals(true, post instanceof Post);
     }
 
     @Test
     public void postInstiatesWithContent_true() throws Exception{
-        Post post = new Post("Day 1: intro");
+        Post post = setUpNewPost();
         assertEquals("Day 1: intro", post.getContent());
     }
 
     @Test
     public void allPostsAreCorrectlyReturned_true() {
-        Post newPost = new Post("Dy 1: intro");
+        Post newPost = setUpNewPost();
         Post otherPost = new Post("How to learn effectively");
         assertEquals(2, Post.getAll().size());
     }
 
     @Test
     public void postContainsAllPosts_true() {
-        Post newPost = new Post("Dy 1: intro");
+        Post newPost = setUpNewPost();
         Post otherPost = new Post("How to learn effectively");
         assertTrue(Post.getAll().contains(newPost));
         assertTrue(Post.getAll().contains(otherPost));
+    }
+
+    @Test
+    public void getPublished_isFalseAfterInstantiation_false() {
+        Post newPost = setUpNewPost();
+        assertEquals(false, newPost.getPublished());
+    }
+
+    @Test
+    public void getCreatedAt_instantiatesWithCurrentTime_today() {
+        Post newPost = setUpNewPost();
+        assertEquals(LocalDateTime.now().getDayOfWeek(), newPost.getCreatedAt().getDayOfWeek());
+    }
+
+    @Test
+    public void getId_postsInstantiateWithAnID_1() throws Exception{
+
+        Post.clearAll();  // Remember, the test will fail without this line! We need to empty leftover Posts from previous tests!
+
+        Post myPost = new Post("Day 1: Intro");
+
+        assertEquals(1, myPost.getId());
     }
 }
